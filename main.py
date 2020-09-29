3876e03110e915c0d1af37aad28037b5bc037098
kivutibrian <briankivuti1@gmail.com>
2020-09-29 21:42:34 +0300

    select & insert query using psycopg2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # import db connection
 from settings.db_connect import conn
@@ -17,8 +17,23 @@ def about():
 def contact():
     return render_template('about.html')
 
-@app.route('/inventories')
+@app.route('/inventories', methods=['GET', 'POST'])
 def inventories():
+
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+
+    if request.method == 'POST':
+        name = request.form['name']
+        type = request.form['type']
+        buying_price = request.form['bp']
+        sp = request.form['sp']
+
+        cur.execute('INSERT INTO inventories (name, type, bp, sp) VALUES (%s,%s,%s,%s)', (name,type,buying_price,sp))
+        conn.commit()
+
+
     return render_template('inventories.html')
     
 @app.route('/charts')
